b39700dfe19623286e06ffe5e14ad5c28439c0a1
MrDJAVELIN <mrdjavelin@gmail.com>
2026-09-13 11:51:37 +0300

    Initial template

---
+++ src/app/(main)/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div className="h-screen flex items-center justify-center gap-1">
+      Next Template by
+      <a href="https://github.com/mrdjavelin" className="underline">
+        DJAVELIN
+      </a>
+    </div>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,12 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import { Geist, Geist_Mono, Inter } from "next/font/google";
+import "@/styles/globals.css";
+import { cn } from "@/lib/utils";
+import { Providers } from "@/components/providers";
+
+const geistHeading = Geist({ subsets: ["latin"], variable: "--font-heading" });
+
+const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -21,9 +27,19 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={cn(
+        "h-full",
+        "antialiased",
+        geistSans.variable,
+        geistMono.variable,
+        "font-sans",
+        inter.variable,
+        geistHeading.variable,
+      )}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <Providers>{children}</Providers>
+      </body>
     </html>
   );
 }
